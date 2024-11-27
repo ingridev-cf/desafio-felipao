16f1a572295b8faaef1289a99c7823bf88955ccf
ingridev-cf <ingrid.melo27@gmail.com>
2024-11-27 16:21:56 -0300

    projeto criado

---
+++ src/index.js
@@ -0,0 +1,45 @@
+let nomeHeroi = "Eduardinho Berserker"
+let xpHeroi = "5200"
+let resultado = ("O herói " + nomeHeroi + " tem " + xpHeroi + " XP e está no nível ")
+
+if (xpHeroi <= 1000){
+    let nivelHeroi1 = "Ferro"
+    console.log(resultado + nivelHeroi1)
+
+}else if(xpHeroi >= 1001 && xpHeroi <= 2000){
+    let nivelHeroi2 = "Bronze"
+    console.log(resultado + nivelHeroi2)
+
+}else if(xpHeroi >= 2001 && xpHeroi <= 5000){
+    let nivelHeroi3 = "Prata"
+    console.log(resultado + nivelHeroi3)
+
+}else if(xpHeroi >= 5001 && xpHeroi <= 7000){
+    let nivelHeroi4 = "Ouro"
+    console.log(resultado + nivelHeroi4)
+
+}else if(xpHeroi >= 7001 && xpHeroi <= 8000){
+    let nivelHeroi5 = "Platina"
+    console.log(resultado + nivelHeroi5)
+
+}else if(xpHeroi >= 8001 && xpHeroi <= 9000){
+    let nivelHeroi6 = "Ascendente"
+    console.log(resultado + nivelHeroi6)
+
+}else if(xpHeroi >= 9001 && xpHeroi <= 10000){
+    let nivelHeroi7 = "Imortal"
+    console.log(resultado + nivelHeroi7)
+
+}else if(xpHeroi >= 10001){
+    let nivelHeroi8 = "Radiante"
+    console.log(resultado + nivelHeroi8)
+}
+
+
+
+
+
+
+
+
+
